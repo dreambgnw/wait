1eecbb590be37fdde963a1803b6c3f0a52c0269b
dreambgnw10 <jardsocial@proton.me>
2025-12-10 07:14:08 +0900

    modified:   js/nav-loader.js

--- js/nav-loader.js
+++ js/nav-loader.js
@@ -108,12 +108,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     Lounge.
                 </div>
 
-                <div class="flex items-center gap-6">
-                    <button onclick="window.toggleSearch()" class="icon-btn" aria-label="Search">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </button>
                     
                     <button id="menu-btn" onclick="window.toggleMenu()" class="icon-btn z-50" aria-label="Menu">
                         <div class="w-6 flex flex-col items-end gap-1.5 transition-all duration-300">
